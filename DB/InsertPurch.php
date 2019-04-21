5ebc7e66360937d49adee218fb4997cef8cf543c
TheAlejandRo <alejandrocaal44@hotmail.com>
2019-04-21 17:18:07 -0600

    Preview Purchases

--- DB/InsertPurch.php
+++ DB/InsertPurch.php
@@ -138,7 +138,7 @@
                 }
 
             } else {
-                echo "Error al insertar la compra.";
+                echo "Error al insertar la compra."; 
             }
     }
 insertProd();
