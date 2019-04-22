f84630787701e6d8b0a82b42d8175c68834a4b30
TheAlejandRo <alejandrocaal44@hotmail.com>
2019-04-21 18:32:52 -0600

    Preview Purchases

--- JS/SelectProduct.js
+++ JS/SelectProduct.js
@@ -1,78 +1,81 @@
 $(document).on("click", "#BselectIn", function(){
 
     var IdProd = $(this).data("id_prod");
+    var NewProd = document.getElementById("NewProductQuery");
 
-    $.ajax({
-        type: 'POST',
-        url: '../DB/SprodD.php',
-        data: {'IdProd': IdProd}
-    })
-    .done(function(resultado){
-        $('#ProductName').val(resultado);
-    })
-    .fail(function(){
-        $('#ProductName').val("No se pudo obtener el producto");
-    })
-
-    $.ajax({
-        type: 'POST',
-        url: '../DB/ScatnP.php',
-        data: {'IdProd': IdProd}
-    })
-    .done(function(resultado){
-        $('#PricePurch').html(resultado);
-    })
-    .fail(function(){
-        $('#PricePurch').html("No se pudo leer el precio de compra...");
-    })
-
-    $.ajax({
-        type: 'POST',
-        url: '../DB/ScatnS.php',
-        data: {'IdProd': IdProd}
-    })
-    .done(function(resultado){
-        $('#PriceSold').html(resultado);
-    })
-    .fail(function(){
-        $('#PriceSold').html("Error al leer precio de venta...");
-    })
-
-    $.ajax({
-        type: 'POST',
-        url: '../DB/ScantSTK.php',
-        data: {'IdProd': IdProd}
-    })
-    .done(function(resultado){
-        $('#StockPL').html(resultado);
-    })
-    .fail(function(){
-        $('#StockPL').html("Error al leer el Stock del Producto...");
-    })
-
-    $.ajax({
-        type: 'POST',
-        url: '../DB/SFex.php',
-        data: {'IdProd': IdProd}
-    })
-    .done(function(resultado){
-        $('#FexpireL').html(resultado);
-    })
-    .fail(function(){
-        $('#FexpireL').html("Error al leer la fecha de expiración del producto...");
-    })
-
-    $.ajax({
-        type: 'POST',
-        url: '../DB/SCP.php',
-        data: {'IdProd': IdProd}
-    })
-    .done(function(resultado){
-        $('#Cproduct').html(resultado);
-    })
-    .fail(function(){
-        $('#Cproduct').html("Error al leer la cantidad en Inventario");
-    })
-
-    $('#idP').html(IdProd);
+    if (!NewProd.checked) {
+        $.ajax({
+            type: 'POST',
+            url: '../DB/SprodD.php',
+            data: {'IdProd': IdProd}
+        })
+        .done(function(resultado){
+            $('#ProductName').val(resultado);
+        })
+        .fail(function(){
+            $('#ProductName').val("No se pudo obtener el producto");
+        })
+    
+        $.ajax({
+            type: 'POST',
+            url: '../DB/ScatnP.php',
+            data: {'IdProd': IdProd}
+        })
+        .done(function(resultado){
+            $('#PricePurch').html(resultado);
+        })
+        .fail(function(){
+            $('#PricePurch').html("No se pudo leer el precio de compra...");
+        })
+    
+        $.ajax({
+            type: 'POST',
+            url: '../DB/ScatnS.php',
+            data: {'IdProd': IdProd}
+        })
+        .done(function(resultado){
+            $('#PriceSold').html(resultado);
+        })
+        .fail(function(){
+            $('#PriceSold').html("Error al leer precio de venta...");
+        })
+    
+        $.ajax({
+            type: 'POST',
+            url: '../DB/ScantSTK.php',
+            data: {'IdProd': IdProd}
+        })
+        .done(function(resultado){
+            $('#StockPL').html(resultado);
+        })
+        .fail(function(){
+            $('#StockPL').html("Error al leer el Stock del Producto...");
+        })
+    
+        $.ajax({
+            type: 'POST',
+            url: '../DB/SFex.php',
+            data: {'IdProd': IdProd}
+        })
+        .done(function(resultado){
+            $('#FexpireL').html(resultado);
+        })
+        .fail(function(){
+            $('#FexpireL').html("Error al leer la fecha de expiración del producto...");
+        })
+    
+        $.ajax({
+            type: 'POST',
+            url: '../DB/SCP.php',
+            data: {'IdProd': IdProd}
+        })
+        .done(function(resultado){
+            $('#Cproduct').html(resultado);
+        })
+        .fail(function(){
+            $('#Cproduct').html("Error al leer la cantidad en Inventario");
+        })
+    
+        $('#idP').html(IdProd);
+    }
 })
